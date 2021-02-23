138c0a1e1db7dfb8a1d638ff2cb020a89beedbee
arthurmonsalless <arthurmonsalless@users.noreply.github.com>
2021-02-23 17:04:20 -0300

    atualizando projeto episódio 2

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,11 @@
-import express from 'express';
+import "reflect-metadata";
+import express from "express";
+import "./database/index";
+import { router } from "./router";
 
 const app = express();
 
-//Get => Busca
-//Post => Salvar
-//Put => Alterar
-//Delete => Deletar
-//Patch => Alterar especifica
-
-// http://localhost:3333/users
-
-app.get("/", (request, response) => {
-    return response.json({ message: "Hello Word - NLW04 "});
-});
-
-// 1 param => Rota(Recurso API)
-// 2 param => request, response
-
-app.post("/", (request, response) => {
-    //Recebeu os dadoos para salvar
-    return response.json({message: "Os dados foram salvos com sucessoo" });
-});
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => console.log("Server is running!"));
